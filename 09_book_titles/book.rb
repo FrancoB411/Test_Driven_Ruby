fba357531c11c893b6c870810531295788f82dca
FrancoB411 <Francob411@gmail.com>
2013-01-19 20:12:52 -0500

    refactor book.rb

--- 09_book_titles/book.rb
+++ 09_book_titles/book.rb
@@ -1,8 +1,6 @@
 class Book
   attr_accessor :title
 
-  SKIP_WORDS = [ "and", "in", "the", "of", "a", "an" ]
-
   def title
     titleize(@title)
   end
@@ -10,17 +8,17 @@ class Book
 private
 
   def titleize(title)
-    title_words = make_word_array(title.capitalize)
+    title_words = title.capitalizetitle.split(" ")
     titleized = title_words.collect { |word| capitalize_or_not(word) }
     titleized.join(" ")
   end
 
-  def make_word_array(title)
-    title.split(" ")
+  def capitalize_or_not(word)
+    skip_words.include?(word) ? word : word.capitalize
   end
 
-  def capitalize_or_not(word)
-    SKIP_WORDS.include?(word) ? word : word.capitalize
+  def skip_words
+    [ "and", "in", "the", "of", "a", "an" ]
   end
 
 end
